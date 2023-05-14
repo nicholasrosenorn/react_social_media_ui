42f0f0b067fd3f73a0dd29b3a20fe8aeb20d7f0b
nicholasrosenorn <nrosenor@purdue.edu>
2023-05-13 21:47:21 -0500

    login page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Login from "./pages/login/Login.jsx"
+
+function App() {
+  return (
+
+    <div>
+      <Login />
+    </div>
+  
+  );
+}
+
+export default App;
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./login.scss"
+
+const Login = () => {
+
+    return (
+        <div className="login">
+            <div className="card">
+                <div className="left">
+                    <h1>Hello World.</h1>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut. 
+                    </p>
+                    <span>Don't have an account?</span>
+                    <button>Register</button>
+                </div>
+                <div className="right">
+                    <h1>Login</h1>
+                    <form>
+                        <input type="text" placeholder="Username" />
+                        <input type="password" placeholder="Password"/>
+                        <button>Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Login
