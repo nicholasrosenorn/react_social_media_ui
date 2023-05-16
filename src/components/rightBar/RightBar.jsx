a26fb656e0b4d68752fd78761d2cdc54ba5d396d
nicholasrosenorn <nrosenor@purdue.edu>
2023-05-16 07:28:00 -0500

    right bar

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -1,10 +1,114 @@
 import React from "react";
 import "./rightbar.scss"
+import User from "../../assets/user_icon_img.png"
 
 const RightBar = () => {
 
     return (
-        <div className="rightbar">rightbar</div>
+        <div className="rightbar">
+            <div className="container">
+                <div className="item">
+                    <span>Suggestions for you</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={User} alt="" />
+                            <span>Jane Doe</span>
+                        </div>
+                        <div className="buttons">
+                            <button>Follow</button>
+                            <button>Dismiss</button>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={User} alt="" />
+                            <span>Jane Doe</span>
+                        </div>
+                        <div className="buttons">
+                            <button>Follow</button>
+                            <button>Dismiss</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="item">
+                    <span>Latest Activities</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={User} alt="" />
+                            <p>
+                                <span>Jane Doe</span> changed their cover photo
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={User} alt="" />
+                            <p>
+                                <span>Jane Doe</span> changed their cover photo
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={User} alt="" />
+                            <p>
+                                <span>Jane Doe</span> changed their cover photo
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={User} alt="" />
+                            <p>
+                                <span>Jane Doe</span> changed their cover photo
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="item">
+                    <span>Online Friends</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={User} alt="" />
+                            <div className="online" />
+                            <span>Jane Doe</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={User} alt="" />
+                            <div className="online" />
+                            <span>Jane Doe</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={User} alt="" />
+                            <div className="online" />
+                            <span>Jane Doe</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={User} alt="" />
+                            <div className="online" />
+                            <span>Jane Doe</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={User} alt="" />
+                            <div className="online" />
+                            <span>Jane Doe</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 
 }
